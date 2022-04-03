806a1fe202ca38a63b84c6a6db97c16c90eea439
Sagrel <aoaee22@gmail.com>
2022-04-03 18:47:32 +0200

    Actualizada pestaña de logros
    Ahora el progreso se muestra de forma más agradable

--- src/routes/Achivements.jsx
+++ src/routes/Achivements.jsx
@@ -1,35 +1,55 @@
-const Achivements = () => {
-    let achivements = [
-        { name: "", description: "Repasa 7 días seguidos", progres: 5, total: 7 },
-        { name: "", description: "No te saltes ningun repaso durante un mes", progres: 2, total: 30 },
-        { name: "", description: "Aprende 20 palabras", progres: 35, total: 20 },
-        { name: "", description: "Aprende 50 palabras", progres: 35, total: 50 },
-        { name: "", description: "Aprende 100 palabras", progres: 35, total: 100 },
-    ]
+import { Center, RingProgress, SimpleGrid, Stack, Text, ThemeIcon } from "@mantine/core"
+import { Check } from 'tabler-icons-react';
+
+const Achivement = ({ name, description, progress, total }) => {
+    const p = Math.min(progress, total);
+    const percentage = Math.round(p / total * 100);
+
+    let ring = null;
+    if (p == total) {
+        ring = (<RingProgress
+            sections={[{ value: 100, color: 'teal' }]}
+            label={
+                <Center>
+                    <ThemeIcon color="teal" variant="light" radius="xl" size="xl">
+                        <Check size={22} />
+                    </ThemeIcon>
+                </Center>
+            }
+        />)
+    } else {
+        ring = (<RingProgress
+            sections={[{ value: percentage, color: 'blue' }]}
+            label={
+                <Text color="blue" weight={700} align="center" size="xl">
+                    {percentage}%
+                </Text>
+            }
+        />)
+    }
 
     return (
-        <div style={{ display: "flex", flexDirection: "column", gap: "2em" }}>
-            {
-                achivements.map((e) => {
-                    const p = Math.min(e.progres, e.total);
+        <Stack align="center">
+            <Text size="xl">{name}</Text>
+            {ring}
+            <Text>{description}</Text>
+        </Stack>
+    )
+}
 
-                    return <div key={e.description}>
-                        <h4>{e.name}</h4>
-                        <p>{e.description}</p>
-                        <div style={{ display: "flex" }}>
-                            <div style={{ width: "80%", backgroundColor: "red", borderRadius: "25px" }}>
-                                <div style={{ width: (p / e.total * 100) + "%", height: "100%", borderRadius: "25px", backgroundColor: "chartreuse" }}>
-                                </div>
-                            </div>
-                            <div style={{ width: "20%", textAlign: "center" }}>
-                                {p === e.total ? "Completado" : p + "/" + e.total}
-                            </div>
-                        </div>
-
-                    </div>
-                })
-            }
-        </div>
+const Achivements = () => {
+    let achivements = [
+        { name: "Racha de dias", description: "Repasa 7 días seguidos", progress: 5, total: 7 },
+        { name: "Constancia", description: "No te saltes ningun repaso durante un mes", progress: 2, total: 30 },
+        { name: "Novato", description: "Aprende 20 palabras", progress: 35, total: 20 },
+        { name: "Intermedio", description: "Aprende 50 palabras", progress: 35, total: 50 },
+        { name: "Nativo", description: "Aprende 100 palabras", progress: 35, total: 100 },
+    ]
+    
+    return (
+        <SimpleGrid cols={4} >
+            {achivements.map((e) => <Achivement {...e} key={e.name}></Achivement>)}
+        </SimpleGrid>
     )
 
 }
