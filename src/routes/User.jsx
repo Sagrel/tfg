797233ea8770bbd76b43384db457f824cfd43af9
Sagrel <aoaee22@gmail.com>
2022-04-03 18:46:33 +0200

    Actualizada pestaña de usuario
    Ahora se muestra el nombre del usuario y su foto de perfil

--- src/routes/User.jsx
+++ src/routes/User.jsx
@@ -1,17 +1,22 @@
-import { Button } from "@mantine/core";
+import { Avatar, Button, Group, Text } from "@mantine/core";
 import { getAuth } from "firebase/auth";
+import { Logout } from "tabler-icons-react";
 
 const User = () => {
 
-
+    const user = getAuth().currentUser;
 
     return (<>
+        <Group>
+            <Avatar src={user.photoURL}></Avatar>
+            <Text>{user.displayName}</Text>
+        </Group>
         <p>
             Aqui podriamos poner informacion como el número de palabras aprendidas,
             opciones para cambiar la contraseña o cualquier otra cosa relacionada con el usuario
 
         </p>
-        <Button onClick={() => {
+        <Button rightIcon={<Logout></Logout>} onClick={() => {
             getAuth().signOut();
         }}>Cerrar sesión</Button>
     </>
