3546641c95407f5c630bb4c04f55de84da035374
Sagrel <aoaee22@gmail.com>
2022-04-15 18:19:16 +0200

    Removed unnecessary setting

--- src/routes/Settings.jsx
+++ src/routes/Settings.jsx
@@ -1,8 +1,9 @@
-import { ActionIcon, Button, Grid, Group, Input, NumberInput, Slider, Stack, Text, useMantineColorScheme } from "@mantine/core";
+import { ActionIcon, Button, Group, NumberInput, Stack, Text, useMantineColorScheme } from "@mantine/core";
 import { SunIcon, MoonIcon } from '@modulz/radix-icons';
 import { getAuth } from "firebase/auth";
-import { collection, doc, getDoc, getFirestore, updateDoc } from "firebase/firestore";
+import { doc, getDoc, getFirestore, updateDoc } from "firebase/firestore";
 import { useState, useEffect } from "react";
+
 const Settings = () => {
 
     const { colorScheme, toggleColorScheme } = useMantineColorScheme();
@@ -18,8 +19,6 @@ const Settings = () => {
         const user_data = await getDoc(userRef);
         setTime(user_data.data().timer);
     }, [])
-
-
 
     return (
         <Stack style={{ height: "100vh", width: "70vw" }}>
@@ -47,17 +46,6 @@ const Settings = () => {
                 }} max={100}
                     min={0}></NumberInput>
             </Group>
-            { /* TODO make this work some how https://www.reddit.com/r/reactjs/comments/sp4pid/how_to_set_responsive_values_for_titles_and_texts/ */}
-            <p>
-                <Text>Cambiar tamaño de letra</Text>
-                <Slider
-                    label={(val) => MARKS.find((mark) => mark.value === val).label}
-                    defaultValue={50}
-                    step={25}
-                    marks={MARKS}
-                    styles={{ markLabel: { display: 'none' } }}
-                />
-            </p>
             <Button disabled={!chaged} onClick={async () => {
                 const db = getFirestore();
                 const userRef = doc(db, "users", getAuth().currentUser.uid);
@@ -69,13 +57,4 @@ const Settings = () => {
         </Stack>
     )
 }
-
-const MARKS = [
-    { value: 0, label: 'xs' },
-    { value: 25, label: 'sm' },
-    { value: 50, label: 'md' },
-    { value: 75, label: 'lg' },
-    { value: 100, label: 'xl' },
-];
-
 export default Settings;
