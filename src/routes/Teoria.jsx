fbfd6c3619a1341fccea0e5604d530445c73be93
Sagrel <aoaee22@gmail.com>
2022-04-22 17:45:48 +0200

    Update Teoria.jsx
    Ahora si cargamos la información de la base de datos. Se muestra con un <Accordion>, queda bonito, pero está sujeto a cambio

--- src/routes/Teoria.jsx
+++ src/routes/Teoria.jsx
@@ -1,15 +1,43 @@
 import { useParams } from "react-router-dom"
+import { useState, useEffect } from "react"
+import { collection, doc, getDocs, getFirestore } from "firebase/firestore"
+import { Accordion, Center, Paper, ScrollArea } from "@mantine/core"
+import { RichTextEditor } from '@mantine/rte';
+import { getAuth } from "firebase/auth";
 
-// TODO Load the notes of the level and display them
-// TODO Allow the user to add their own notes
 const Teoria = () => {
 	const { tema } = useParams()
 
-	// user/user_id/mazos/mazo_id/notas
-	let html = "Teoria  sobre " + tema + ", puede contener html como esto <div style=\"background-color:red;\">Hola mundo</div> asi podemos incluir imagenes y enlaces a nuestro antojo"
+	const [notas, setNotas] = useState([])
 
-	return (<div dangerouslySetInnerHTML={{ __html: html }}></div>)
+	useEffect(async () => {
+		const user = getAuth().currentUser;
+		if (!user) return;
+		const db = getFirestore();
+		const notasRef = collection(db, "users", user.uid, "mazos", tema, "notas");
+		const notas = await getDocs(notasRef);
+		setNotas(notas.docs.map(doc => ({ id: doc.id, ...doc.data() })))
+	}, [])
 
+	return (
+		<Paper style={{ width: "100vw", height: "100vh" }} radius={0}>
+			<Center>
+
+				<ScrollArea style={{ height: "100vh", width: "80vw" }} type="never">
+					<h1>Notas del mazo</h1>
+					<Accordion>
+						{notas.map((note) => {
+							return (
+								<Accordion.Item label={note.title} key={note.id}>
+									<RichTextEditor value={note.content} readOnly />
+								</Accordion.Item>
+							)
+						})}
+					</Accordion>
+				</ScrollArea>
+			</Center>
+		</Paper>
+	)
 }
 
 export default Teoria
